580a75f3c0b5c44c02f8a704d02e6c58abd6d2a8
YDJ <nextdj@163.com>
2014-11-21 12:48:42 +0800

    add source to project

--- ExtSDK/ExtSDK/NSArray+ExtensionExt.h
+++ ExtSDK/ExtSDK/NSArray+ExtensionExt.h
@@ -1,6 +1,6 @@
 //  NSArray+ExtensionExt.h
 //
-//  Copyright (c) 2014 YDJ (ExtSDK) (https://github.com/ydj/ExtSDK)
+//  Copyright (c) 2014 NextDj (https://github.com/ydj/ExtSDK)
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
--- ExtSDK/ExtSDK/UIImage+ExtensionExt.m
+++ ExtSDK/ExtSDK/UIImage+ExtensionExt.m
@@ -1,6 +1,6 @@
 //  UIImage+ExtensionExt.m
 //
-//  Copyright (c) 2014 YDJ (ExtSDK) (https://github.com/ydj/ExtSDK)
+//  Copyright (c) 2014 NextDj (https://github.com/ydj/ExtSDK)
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
