c111bd58e8161fccfed3bf4f7e303738284da129
YDJ <nextdj@163.com>
2014-11-21 13:18:04 +0800

    add links

--- ExtSDK/ExtSDK/UIDevice+ExtensionExt.m
+++ ExtSDK/ExtSDK/UIDevice+ExtensionExt.m
@@ -1,6 +1,6 @@
 //  UIDevice+ExtensionExt.m
 //
-//  Copyright (c) 2014 YDJ (https://github.com/ydj/ExtSDK)
+//  Copyright (c) 2014 YDJ ( https://github.com/ydj/ExtSDK )
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
