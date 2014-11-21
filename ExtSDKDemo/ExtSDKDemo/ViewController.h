44c665dcdad3a9629f50a8134ab0cb28aba3ffe5
YDJ <nextdj@163.com>
2014-11-21 12:59:03 +0800

    ADD  MIT LICESE

--- ExtSDKDemo/ExtSDKDemo/ViewController.h
+++ ExtSDKDemo/ExtSDKDemo/ViewController.h
@@ -3,7 +3,7 @@
 //  ExtSDKDemo
 //
 //  Created by YDJ on 14/11/21.
-//  Copyright (c) 2014年 nextdj. All rights reserved.
+//  Copyright (c) 2014年 YDJ. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
